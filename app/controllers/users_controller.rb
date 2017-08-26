8af97fc7a716e294c6f177fd50cf5b3f75391133
osmarluz <osmar.luz@gmail.com>
2017-08-26 19:17:11 +0000

    Restarting the repository from scratch

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+  
+  def index
+    @users = User.all
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated succesfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def show
+  end
+  
+  private
+  def set_user
+      @user = User.find(params[:id])
+  end
+    
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
